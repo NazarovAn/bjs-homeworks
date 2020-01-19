3666e3b53ed529a1b6db0535b2d5153cad178939
NazarovAn <naatonchrome@gmail.com>
2020-01-19 23:48:54 +0400

    Homework 1.1 done

---
+++ 1.1-variables/main.js
@@ -0,0 +1,26 @@
+//Задание 1
+
+let algebra = 4;
+let geography = 5;
+let physics = 4;
+
+let averageScore = (algebra + geography + physics) / 3;
+
+console.log(`Средняя оценка за три предмета ${averageScore.toFixed(2)}`);
+
+//Задание 2
+
+let myName = 'Антон';
+let message = `Привет, мир! Меня зовут ${myName}.`;
+
+console.log(message);
+
+//Задание 3
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(`x * y + 5 * z + x - 1 = ${result}`);
