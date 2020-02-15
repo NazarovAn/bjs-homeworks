1cc6727e367b8fd877bae58a02004dae7c14f244
NazarovAn <naatonchrome@gmail.com>
2020-02-16 01:36:31 +0400

    HW 3.2 corrections

--- 3.2-async/task.js
+++ 3.2-async/task.js
@@ -3,7 +3,7 @@
 class AlarmClock{
     constructor () {
         this.alarmCollection = [];
-        this.id;
+        this.timerId = null;
     }
 
     isOn(value){
@@ -12,11 +12,13 @@ class AlarmClock{
 
     addClock(time, callbackFn, callId){
         if(callId === undefined){
-            return console.error(`Нет ID звонка`);
+            console.error(`Нет ID звонка`);
+            return;
         }
 
         if(this.isOn(callId)){
-            return console.error(`Такой будильник уже есть`);
+            console.error(`Такой будильник уже есть`);
+            return;
         }
 
         console.log(`Будильник №${callId} на ${time} создан`);
@@ -37,48 +39,54 @@ class AlarmClock{
 
     getCurrentFormattedTime(){
         const today = new Date();
-        return `${today.getHours()}:${today.getMinutes()}`
+        let minute = today.getMinutes(),
+            hour = today.getHours();
+
+        if(minute < 10){
+            minute = `0` + minute;
+        }
+
+        if(hour < 10){
+            hour = `0` + hour;
+        }
+
+        return `${hour}:${minute}`
     }
 
     start(){
-        function checkClock(time){
-            const today = new Date();
-            const now = `${today.getHours()}:${today.getMinutes()}`;
-
-            if(now == time){
-                return true;
-            } else{
-                return false;
-            }
+        function checkClock(time, now){
+            return now == time
         }
 
         const intervalId = setInterval(() => {
-            this.alarmCollection.some(element => {
-                if(checkClock(element.time) === true){
+            this.alarmCollection.forEach(element => {
+                if(checkClock(element.time, this.getCurrentFormattedTime()) === true){
                     return element.callbackFn();
                 }
             });            
         }, 1000);
 
-        return this.id = intervalId;
+        return this.timerId = intervalId;
     }
 
     stop(){
-        if(this.id != undefined){
-            clearInterval(this.id);
+        if(this.timerId != null){
+            clearInterval(this.timerId);
         }
 
         console.log('Все будильники выключены');
 
-        return this.id = undefined;
+        return this.timerId = null;
     }
 
     printAlarms(){
         this.alarmCollection.forEach(element => console.log(`Время будильника: ${element.time}\nНомер будильника: ${element.callId}`));
 
-        if(this.alarmCollection === []){
+        if(this.alarmCollection.length === 0){
             console.log('Будильников нет.')
         }
+
+        return;
     }
 
     clearAlarms(){
@@ -86,16 +94,16 @@ class AlarmClock{
 
         this.alarmCollection = [];
 
-        return setTimeout(() => {console.log(`Поставить новый будильник?`)});
+        return;
     }
 }
 
 const testCase = new AlarmClock;
 
-testCase.addClock('1:40', () =>{console.log(`Пора спать`)}, 1);
-testCase.addClock('1:41', () =>{console.log(`Пора уже`); testCase.removeClock(2)}, 2);
-testCase.addClock('1:41', () => console.log(`Пора`));
-testCase.addClock('1:42', () =>{console.log(`Спааать!`); testCase.clearAlarms(); testCase.printAlarms()}, 3);
-testCase.addClock('1:43', () =>console.log(`Sleep!`), 1);
+testCase.addClock('00:33', () =>{console.log(`Пора спать`)}, 1);
+testCase.addClock('00:34', () =>{console.log(`Пора уже`); testCase.removeClock(2)}, 2);
+testCase.addClock('00:34', () => console.log(`Пора`));
+testCase.addClock('00:35', () =>{console.log(`Спааать!`); testCase.clearAlarms(); testCase.printAlarms()}, 3);
+testCase.addClock('00:36', () =>console.log(`Sleep!`), 1);
 testCase.printAlarms();
 testCase.start();
